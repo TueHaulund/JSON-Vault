b6620945ce9ad1432a5d204dd8d615d9c5aba35c
Tue Haulund <tue.haulund@gmail.com>
2017-11-11 14:45:06 +0100

    Added PostgreSQL DB and implemented store/fetch tasks

--- json-vault.py
+++ json-vault.py
@@ -9,18 +9,19 @@ def index():
 
 @app.route('/api/v1.0/json', methods=['GET'])
 def get_json():
-    return flask.make_response(tasks.fetch(), 200, {'Content-Type': 'application/json'})
+    return flask.jsonify(tasks.fetch())
 
 @app.route('/api/v1.0/json', methods=['POST'])
 def post_json():
-    json = flask.request.get_json()
-
-    if not json:
+    if not flask.request.is_json:
         flask.abort(400)
 
-    tasks.broadcast(json)
-    tasks.store(json)
-    return flask.make_response(flask.jsonify(json), 201)
+    #We should check payload length before doing this
+    json_string = flask.request.get_data().decode("utf-8")
+
+    tasks.broadcast(json_string)
+    tasks.store(json_string)
+    return flask.make_response(json_string, 201)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tasks.py
+++ tasks.py
@@ -1,9 +1,36 @@
+import psycopg2
+import itertools
+import os
+import sys
+
+try:
+    params = {
+        'dbname': 'json-vault',
+        'host': 'localhost',
+        'user': os.environ['PSQL_USER'],
+        'password': os.environ['PSQL_PW']
+    }
+
+    conn = psycopg2.connect(**params)
+except:
+    print("Could not connect to PostgreSQL")
+    sys.exit()
+
 def store(json):
-	print('Storing: %s' % json)
+    print('Storing: %s' % json)
+    cur = conn.cursor()
+    cur.execute('INSERT INTO json(data) VALUES (%s)', (json,))
+    cur.close()
+    conn.commit()
 
 def fetch():
-	print('Fetching')
-	return '{"example": "data"}'
+    print('Fetching')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM json')
+    raw_data = cur.fetchall()
+    cur.close()
+    return list(itertools.chain(*raw_data)) #Collapse list of tuples to plain list
 
 def broadcast(json):
-	print('Broadcasting: %s' % json)
+    print('Broadcasting: %s' % json)
+    #TODO: Broadcast over WebSockets
