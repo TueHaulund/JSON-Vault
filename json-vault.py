06911a30f5b12cb2885a0dc52b51ac776d4b02d7
Tue Haulund <tue.haulund@gmail.com>
2017-11-11 13:13:48 +0100

    Fleshed out app structure with API endpoints and tasks

--- json-vault.py
+++ json-vault.py
@@ -1,10 +1,26 @@
-from flask import Flask
+import flask
+import tasks
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return app.send_static_file('index.html')
+
+@app.route('/api/v1.0/json', methods=['GET'])
+def get_json():
+    return flask.make_response(tasks.fetch(), 200, {'Content-Type': 'application/json'})
+
+@app.route('/api/v1.0/json', methods=['POST'])
+def post_json():
+    json = flask.request.get_json()
+
+    if not json:
+        flask.abort(400)
+
+    tasks.broadcast(json)
+    tasks.store(json)
+    return flask.make_response(flask.jsonify(json), 201)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tasks.py
@@ -0,0 +1,9 @@
+def store(json):
+	print('Storing: %s' % json)
+
+def fetch():
+	print('Fetching')
+	return '{"example": "data"}'
+
+def broadcast(json):
+	print('Broadcasting: %s' % json)
